02e8accd7a34978f3dff2af92bb1cc3b0af776ee
Likhon Baroi <likhonhere007@proton.me>
2026-02-28 22:53:15 +0600

    make mobile responsive

--- js/main.js
+++ js/main.js
@@ -15,3 +15,33 @@ document.addEventListener("DOMContentLoaded", async () => {
     initScrollReveal();
     initCertificates();
 });
+
+
+/* ========================================
+   Responsive Adjustments for Navigation
+======================================== */
+const menuToggle = document.getElementById("menu-toggle");
+const navLinksContainer = document.querySelector(".nav-links");
+
+menuToggle?.addEventListener("click", () => {
+    navLinksContainer.classList.toggle("active");
+});
+
+// Close Menu On Link Click
+const navLinks = document.querySelectorAll(".nav-links a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinksContainer.classList.remove("active");
+    });
+});
+
+// Close Menu If Click Outside
+document.addEventListener("click", (e) => {
+    const menu = document.querySelector(".nav-links");
+    const toggle = document.getElementById("menu-toggle");
+
+    if (!menu.contains(e.target) && !toggle.contains(e.target)) {
+        menu.classList.remove("active");
+    }
+});
